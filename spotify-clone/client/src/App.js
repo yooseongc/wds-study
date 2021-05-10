ab2f1f6dd8211b336acb47b951e4f0d5c66702fe
Yooseong Choi <choiys3574@naver.com>
2021-05-11 02:42:00 +0900

    update TIL

---
+++ spotify-clone/client/src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Dashboard from './Dashboard';
+import Login from './Login';
+
+const code = new URLSearchParams(window.location.search).get('code');
+
+function App() {
+    return code ? <Dashboard code={ code } /> : <Login />;
+}
+
+export default App;
