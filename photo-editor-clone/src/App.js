fea7777dbfafc34f06d0137910f513e6dc730d27
Yooseong Choi <choiys3574@naver.com>
2021-05-15 03:56:39 +0900

    update TIL

---
+++ photo-editor-clone/src/App.js
@@ -0,0 +1,124 @@
+import { useState } from 'react';
+import './App.css';
+import SidebarItem from './SidebarItem';
+import Slider from './Slider';
+
+const DEFAULT_OPTIONS = [
+    {
+        name: 'Brightness',
+        property: 'brightness',
+        value: 100,
+        range: {
+            min: 0,
+            max: 200
+        },
+        unit: '%'
+    },
+    {
+        name: 'Contrast',
+        property: 'contrast',
+        value: 100,
+        range: {
+            min: 0,
+            max: 200
+        },
+        unit: '%'
+    },
+    {
+        name: 'Saturation',
+        property: 'saturate',
+        value: 100,
+        range: {
+            min: 0,
+            max: 200
+        },
+        unit: '%'
+    },
+    {
+        name: 'Grayscale',
+        property: 'grayscale',
+        value: 0,
+        range: {
+            min: 0,
+            max: 100
+        },
+        unit: '%'
+    },
+    {
+        name: 'Sepia',
+        property: 'sepia',
+        value: 0,
+        range: {
+            min: 0,
+            max: 100
+        },
+        unit: '%'
+    },
+    {
+        name: 'Hue Rotate',
+        property: 'hue-rotate',
+        value: 0,
+        range: {
+            min: 0,
+            max: 360
+        },
+        unit: 'deg'
+    },
+    {
+        name: 'Blur',
+        property: 'blur',
+        value: 0,
+        range: {
+            min: 0,
+            max: 20
+        },
+        unit: 'px'
+    },
+];
+
+function App() {
+
+    const [selectedOptionIndex, setSelectedOptionIndex] = useState(0);
+    const [options, setOptions] = useState(DEFAULT_OPTIONS);
+
+    const selectedOption = options[selectedOptionIndex];
+
+    function handleSliderChange({ target }) {
+        setOptions(prevOptions => {
+            return prevOptions.map((option, i) => {
+                return (i !== selectedOptionIndex) ? option : { ...option, value: target.value };
+            });
+        })
+    }
+
+    function getImageStyle() {
+        const filters = options.map(option => `${option.property}(${option.value}${option.unit})`);
+        return { filter: filters.join(' ') };
+    }
+
+    return (
+        <div className="container">
+            <div className="main-image" style={ getImageStyle() } />
+            <div className="sidebar">
+                {
+                    options.map((option, i) => (
+                        <SidebarItem 
+                            key={ i } 
+                            name={ option.name } 
+                            active={ i === selectedOptionIndex } 
+                            handleClick={ () => setSelectedOptionIndex(i) }
+                        />
+                    ))
+                }
+            </div>
+            <Slider 
+                min={ selectedOption.range.min }
+                max={ selectedOption.range.max }
+                value={ selectedOption.value }
+                handleChange={ handleSliderChange }
+            />
+        </div>
+    );
+}
+
+export default App;
