ef3f8ecdb57d2e41c74e1b9f8967e879210b2098
Yooseong Choi <choiys3574@naver.com>
2022-05-05 18:23:37 +0900

    UPDATE TIL: countdown flip timer

---
+++ countdown-flip-timer/script.js
@@ -0,0 +1,57 @@
+
+const flipCard = document.querySelector(".flip-card");
+const countToDate = new Date().setHours(new Date().getHours() + 24);
+
+setInterval(() => {
+    const currentDate = new Date();
+    const timeBetweenDates = Math.ceil((countToDate - currentDate) / 1000);
+    flipAllCards(timeBetweenDates);
+}, 250);
+
+
+function flipAllCards(time) {
+    const seconds = time % 60;
+    const minutes = Math.floor(time / 60) % 60;
+    const hours = Math.floor(time / 3600);
+
+    flip(document.querySelector("[data-hours-tens]"), Math.floor(hours / 10));
+    flip(document.querySelector("[data-hours-ones]"), hours % 10);
+    flip(document.querySelector("[data-minutes-tens]"), Math.floor(minutes / 10));
+    flip(document.querySelector("[data-minutes-ones]"), minutes % 10);
+    flip(document.querySelector("[data-seconds-tens]"), Math.floor(seconds / 10));
+    flip(document.querySelector("[data-seconds-ones]"), seconds % 10);
+}
+
+
+function flip(flipCard, newNumber) {
+
+    const topHalf = flipCard.querySelector(".top");
+    const startNumber = parseInt(topHalf.textContent);
+    if (startNumber === newNumber) return;
+
+    const bottomHalf = flipCard.querySelector(".bottom");
+    const topFlip = document.createElement("div");
+    topFlip.classList.add("top-flip");
+    const bottomFlip = document.createElement("div");
+    bottomFlip.classList.add("bottom-flip");
+
+    topHalf.textContent = startNumber;
+    bottomHalf.textContent = startNumber;
+    topFlip.textContent = startNumber;
+    bottomFlip.textContent = newNumber;
+
+    topFlip.addEventListener("animationstart", e => {
+        topHalf.textContent = newNumber;
+    });
+
+    topFlip.addEventListener("animationend", e => {
+        topFlip.remove();
+    });
+
+    bottomFlip.addEventListener("animationend", e => {
+        bottomHalf.textContent = newNumber;
+        bottomFlip.remove();
+    });
+
+    flipCard.append(topFlip, bottomFlip);
+}
